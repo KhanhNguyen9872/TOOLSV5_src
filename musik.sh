e4573380cbb6d7f98889a4b09c49cb5b80c51137
Galirus_Official™ <114099161+Galirus404@users.noreply.github.com>
2024-05-29 11:59:00 +0700

    Update musik.sh

--- musik.sh
+++ musik.sh
@@ -1,1 +1,7 @@
-mpv --volume=60 $HOME/TOOLSV5/music/  &>/dev/null &
+#!/bin/bash
+# Direktori musik
+MUSIC_DIR="$HOME/TOOLSV5/music/"
+VOLUME=60
+while true; do
+    find "$MUSIC_DIR" -type f \( -iname "*.mp3" -o -iname "*.wav" -o -iname "*.flac" \) -print0 | xargs -0 -I{} mpv --volume=$VOLUME "{}"
+done
